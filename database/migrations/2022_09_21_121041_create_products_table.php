165534a4c11902eb223b40a3c6e95c8c562c4259
intellikgh <srintelligh@gmail.com>
2022-09-21 23:14:22 +0930

    added product model with migration

---
+++ database/migrations/2022_09_21_121041_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('sku')->default(0)->comment('SKU');
+            $table->string('name')->comment('Name');
+            $table->string('category')->comment('Category');
+            $table->integer('price')->default(0)->comment('Price');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
